97478045c2b6dc4b1008add38e0f7260156efbae
vagrant <support@codingbrain.com>
2017-04-24 10:20:34 +0100

    solution

--- src/main/java/bookiemarker/api/domain/BookmarkRepository.java
+++ src/main/java/bookiemarker/api/domain/BookmarkRepository.java
@@ -1,7 +1,12 @@
 package bookiemarker.api.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 public interface BookmarkRepository extends CrudRepository<Bookmark, Long>{
-
+    @RestResource(path = "url")
+    List<Bookmark> findByUrlContainingIgnoreCase(@Param("url") String url);
 }
