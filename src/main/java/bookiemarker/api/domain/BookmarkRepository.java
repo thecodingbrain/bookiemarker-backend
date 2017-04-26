5fa4300553eee6f71a3df0fd814c5b9e5f0c8ea8
vagrant <support@codingbrain.com>
2017-04-26 07:12:12 +0100

    cross origin

--- src/main/java/bookiemarker/api/domain/BookmarkRepository.java
+++ src/main/java/bookiemarker/api/domain/BookmarkRepository.java
@@ -3,9 +3,11 @@ package bookiemarker.api.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 public interface BookmarkRepository extends CrudRepository<Bookmark, Long>{
     @RestResource(path = "url")
     List<Bookmark> findByUrlContainingIgnoreCase(@Param("url") String url);
